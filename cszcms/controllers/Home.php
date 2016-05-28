7c13dfdeff70664b1aab936964e2ad127fd5e97d
cskaza <cskaza@CSKAZA-PC>
2016-05-28 19:17:59 +0700

    Fix for Version 1.0.5

--- cszcms/controllers/Home.php
+++ cszcms/controllers/Home.php
@@ -71,8 +71,9 @@ class Home extends CI_Controller {
         //Get pages from database
         $this->template->setSub('page', $this->page_url);
         $this->template->setSub('page_rs', $this->page_rs);
-        
-        $this->output->cache(30);
+        if($this->uri->segment(1)){
+            $this->output->cache(30);
+        }
         //Load the view
         $this->template->loadSub('frontpage/getpage');
     }
