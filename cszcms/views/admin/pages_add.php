71c4c337bc72451b0463dce412797e9a8f244a65
cskaza <cskaza@gmail.com>
2016-07-07 10:03:17 +0700

    Fix for Version 1.0.9

--- cszcms/views/admin/pages_add.php
+++ cszcms/views/admin/pages_add.php
@@ -104,13 +104,13 @@
         <br>
         <div class="control-group">
             <?php
-             $starter_html = '<br><br><div class="container">
+             $starter_html = '<div class="container">
                             <div class="row">
                             <div class="col-md-12">
                             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
                             </div>
                             </div>
-                            </div><p></p>';
+                            </div><br><br>';
             ?>
             <label class="control-label" for="content"><?php echo $this->lang->line('pages_content'); ?></label>
             <textarea name="content" id="content" class="form-control body-tinymce"><?php echo $starter_html?></textarea>
