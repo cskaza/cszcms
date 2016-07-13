cca8ec42659a8598c3fed90e6a8b6fd17b329425
cskaza <cskaza@gmail.com>
2016-07-13 19:39:13 +0700

    Fix for Version 1.0.9

--- cszcms/config/template.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-/* For Default Template */
-$config['template_master']  = 'main'; /* File in View/Templates */
-$config['template_name']  = 'cszdefault'; /* Template default name */
-$config['template_folder']  = 'templates';
-$config['data_container']   = 'content';
