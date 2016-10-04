aea0b04d9fca5c9db2fd41b4f73d21635e444985
cskaza <cskaza@gmail.com>
2016-10-04 13:43:21 +0700

    Fix for Version 1.1.1

--- cszcms/config/session.php
+++ cszcms/config/session.php
@@ -2,6 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /* Session namespace */
-$config['sess_namespace'] = 'cszcms';
+$config['sess_namespace'] = 'cszcms_'.md5(EMAIL_DOMAIN);
 /* End of file session.php */
 /* Location: ./application/config/session.php */
